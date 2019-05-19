2443c752ca3e947038dba7360e1b8f296a9e50fc
Avernethy Francisco <avernethy@gmail.com>
2019-05-19 00:17:23 -0700

    Initial Commit

---
+++ AdjustedClosingPrices.py
@@ -0,0 +1,14 @@
+# taken directly from https://medium.com/python-data/effient-frontier-in-python-34b0c3043314
+# import needed modules
+import quandl
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# get adjusted closing prices of 5 selected companies with Quandl
+quandl.ApiConfig.api_key = 'PASTE YOUR API KEY HERE'
+selected = ['CNP', 'F', 'WMT', 'GE', 'TSLA']
+data = quandl.get_table('WIKI/PRICES', ticker = selected,
+                        qopts = { 'columns': ['date', 'ticker', 'adj_close'] },
+                        date = { 'gte': '2014-1-1', 'lte': '2016-12-31' }, paginate=True)
+data.head()
